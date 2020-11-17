7643d82411db7cb5b36eae7d9962dec7d459a357
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2020-11-17 09:39:38 -0600

    __numeric__.py added

--- likelihood/__init__.py
+++ likelihood/__init__.py
@@ -1,6 +1,6 @@
 """
 Authors: J. A. Moreno-Guerra & J. I. Ávalos-López
-Library version: 1.0.5
+Library version: 1.0.6
 Last modification: 11/17/2020
 Corresponding author: jzs.gm27@gmail.com 
 Auxiliary email: avaloslj2014@licifug.ugto.mx
@@ -9,3 +9,4 @@ Auxiliary email: avaloslj2014@licifug.ugto.mx
 
 from likelihood.__main__ import *
 from likelihood.__tools__ import *
+from likelihood.__numeric__ import *
