0a445d36d12ed07a78dc7bff5b13e7dd6619ca25
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2024-04-11 20:28:48 -0600

    develop (#32)
    * Add `simulation.py`
    
    * Update `requirements.txt`
    
    * Update `simulation.py`
    
    Add  `_clean_data`  method
    Complete `predict` method
    Fix bug on `FeatureSelection` class in `get_digraph` method
    Add `check_nan_inf` function
    
    * Update comments
    
    * Add new version
    
    * Update `graph.py`
    
    Add `pyvis_to_networkx` method.
    
    * Update `graph.py`
    
    [FIX] GitHub Actions workflow
    
    * Add `generate_feature_yaml`
    
    * Update `graph.py`
    
    * Add `self.node_edge_dict` variable
    
    ---------
    
    Co-authored-by: jafetcc02 <114709102+jafetcc02@users.noreply.github.com>

--- likelihood/graph/graph.py
+++ likelihood/graph/graph.py
@@ -1,3 +1,6 @@
+from typing import List
+
+import networkx as nx
 from IPython.display import HTML, display
 from pandas.core.frame import DataFrame
 from pyvis.network import Network
@@ -15,6 +18,7 @@ class DynamicGraph(FeatureSelection):
         self.df = df
         self.n_importances = n_importances
         super().__init__(**kwargs)
+        self.labels: List[str] = []
 
     def fit(self, **kwargs) -> None:
         """Fit the model according to the given data and parameters."""
@@ -36,6 +40,7 @@ class DynamicGraph(FeatureSelection):
     def _add_nodes(self) -> None:
         for i in range(len(self.all_features_imp_graph)):
             node = self.all_features_imp_graph[i][0]
+            self.labels.append(node)
             self.G.add_node(n_id=i, label=node)
 
     def draw(self, name="graph.html", **kwargs) -> None:
@@ -48,3 +53,24 @@ class DynamicGraph(FeatureSelection):
 
         html_file_content = open(name, "r").read()
         display(HTML(html_file_content))
+
+    def pyvis_to_networkx(self):
+        nx_graph = nx.Graph()
+
+        # Adding nodes
+        nodes = [d["id"] for d in self.G.nodes]
+        for node_dic in self.G.nodes:
+            id = node_dic["label"]
+            del node_dic["label"]
+            nx_graph.add_nodes_from([(id, node_dic)])
+        self.node_edge_dict = dict(zip(nodes, self.labels))
+        del nodes
+
+        # Adding edges
+        for edge in self.G.edges:
+            source, target = self.node_edge_dict[edge["from"]], self.node_edge_dict[edge["to"]]
+            del edge["from"]
+            del edge["to"]
+            nx_graph.add_edges_from([(source, target, edge)])
+
+        return nx_graph
