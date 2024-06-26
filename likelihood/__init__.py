5bea87b7181a569c25830b768361da0d00bf7836
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2024-06-25 20:08:42 -0600

    Update docstring for `__init__.py`

--- likelihood/__init__.py
+++ likelihood/__init__.py
@@ -1,6 +1,17 @@
 """
-Authors: J. A. Moreno-Guerra
-Corresponding author: jzs.gm27@gmail.com
+Likelihood: Initialize the Package
+=====================================
+
+This is the entry point of the Likelihood package. It initializes all necessary modules and provides a central hub for accessing various tools and functions.
+
+Main Modules:
+- likelihood.main: Provides access to core functionality, including data preprocessing, model training, and analysis.
+- likelihood.models: Offers pre-built models for AutoEncoder-based classification and regression tasks.
+- likelihood.tools: Contains utility functions for data manipulation, normalization, and visualization.
+
+By importing the main modules directly or accessing them through this central entry point (i.e., `from likelihood import *`), you can leverage the full range of Likelihood's capabilities to streamline your data analysis workflow.
+
+To get started with Likelihood, simply import the desired modules and start exploring!
 """
 
 from likelihood.main import *
