45d553d746ed1368d4d2f7df2903331dbd971c2d
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2025-08-12 17:03:16 +0000

    Create _nn.py

--- likelihood/graph/__init__.py
+++ likelihood/graph/__init__.py
@@ -1,1 +1,9 @@
+import tensorflow as tf
+from packaging import version
+
 from .graph import *
+
+if version.parse(tf.__version__) > version.parse("2.15.0"):
+    from ._nn import *
+else:
+    from .nn import *
