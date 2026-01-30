ded4f4128e550292cacd1b9368ebb0affe9b057e
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2026-01-30 15:19:32 -0600

    [ADD] lib.rs for rust integration
    This is a demo of Rust integration with Python.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn print_hello() {
+    println!("Hello from Rust!");
+}
+
+#[pymodule]
+fn rust_py_integration(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(print_hello, m)?)?;
+    Ok(())
+}
