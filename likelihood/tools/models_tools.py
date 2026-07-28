02ae232aaf8fe3a82351ebc0127b48452dffd9e0
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2026-07-28 17:30:53 -0600

    Update models_tools.py

--- likelihood/tools/models_tools.py
+++ likelihood/tools/models_tools.py
@@ -1394,9 +1394,7 @@ def display_network_analysis(
     display(option_style)
     display(action_style)
 
-    display(
-        HTML(
-            f"""
+    display(HTML(f"""
     <div style="
         margin-top:20px;
         padding:15px;
@@ -1409,9 +1407,7 @@ def display_network_analysis(
         Most variable option dimension: {max_opt_dim}<br>
         Most variable action dimension: {max_act_dim}
     </div>
-    """
-        )
-    )
+    """))
 
 
 if __name__ == "__main__":
